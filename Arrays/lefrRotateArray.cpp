6cca04318ddf834e908acd92f93180fcadbf4bab
Avaneesh Gupta <avaneesh.gupta5@gmail.com>
2025-01-21 22:48:21 +0530

    Left rotate array

---
+++ Arrays/lefrRotateArray.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+
+using namespace std;
+
+void leftRotate(int arr[], int n){
+    int temp = arr[0]; 
+    for(int i = 1; i<n; i++){
+        arr[i-1] = arr[i];
+    }
+    arr[n-1] = temp;
+
+    for (int i = 0; i<n; i++){
+        cout<<arr[i]<<" ";
+    }
+}
+
+int main(){
+    int arr[] = {1,2,3,4,5,6,7};
+
+    leftRotate(arr, sizeof(arr)/sizeof(arr[0]));
+
+    return 0;
+
+}
