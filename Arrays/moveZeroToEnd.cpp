d89164e6be3f38edaa9bcef4c97ba5bbbd364bfc
Avaneesh Gupta <avaneesh.gupta5@gmail.com>
2025-01-22 22:38:42 +0530

    move zeros to the end of the array

---
+++ Arrays/moveZeroToEnd.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+
+void moveZeroToEnd(int arr[], int n ){
+    
+}
+
+int main(){
+
+    int arr[] = {1,0,2,3,2,0,0,4,5,1};
+
+    moveZeroToEnd(arr, sizeof(arr)/sizeof(arr[0]));
+
+    return 0;
+
+}
