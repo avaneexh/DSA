917676a8e9c94f40c50cf8b20eb1961af9444e00
Avaneesh Gupta <avaneesh.gupta5@gmail.com>
2025-01-28 10:07:22 +0530

    move zeros to the end algorithm

--- Arrays/moveZeroToEnd.cpp
+++ Arrays/moveZeroToEnd.cpp
@@ -3,12 +3,34 @@
 using namespace std;
 
 void moveZeroToEnd(int arr[], int n ){
-    int i=0
+    int j=-1;
+    for (int  i = 0; i<n; i++){
+        if (arr[i] == 0)        
+        {
+            j=i;
+            break;
+        }
+               
+    }
+    for(int i = j+1; i<n; i++){
+        if (arr[i] !=0){
+            swap(arr[i], arr[j]);
+            j++;
+            
+        }
+
+    }
+
+    for (int  i = 0; i < n; i++)
+    {
+        cout<<arr[i]<<"  ";
+    }
+    
 }
 
 int main(){
 
-    int arr[] = {1,0,2,3,2,0,0,4,5,1};
+    int arr[] = {0,0,0,1,0,2,3,2,0,0,4,5,1};
 
     moveZeroToEnd(arr, sizeof(arr)/sizeof(arr[0]));
 
