63f98234dbbcb4d9b27f333df5aa9d046f7b1b3f
Avaneesh Gupta <avaneesh.gupta5@gmail.com>
2025-02-02 20:53:44 +0530

    refactor

--- Arrays/moveZeroToEnd.cpp
+++ Arrays/moveZeroToEnd.cpp
@@ -9,31 +9,21 @@ void moveZeroToEnd(int arr[], int n ){
         {
             j=i;
             break;
-        }
-               
+        }          
     }
     for(int i = j+1; i<n; i++){
         if (arr[i] !=0){
             swap(arr[i], arr[j]);
-            j++;
-            
+            j++;   
         }
-
     }
-
     for (int  i = 0; i < n; i++)
     {
         cout<<arr[i]<<"  ";
-    }
-    
+    }   
 }
-
 int main(){
-
     int arr[] = {0,0,0,1,0,2,3,2,0,0,4,5,1};
-
     moveZeroToEnd(arr, sizeof(arr)/sizeof(arr[0]));
-
     return 0;
-
 }
