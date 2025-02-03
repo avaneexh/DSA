405aed00f48ebd319cdc89ffc90dfffee3d140a2
avaneesh3w <avaneesh@triplewsols.com>
2025-02-03 20:27:32 +0530

    remove unnecessary blank lines

--- Arrays/maximumConsecutiveOnes.cpp
+++ Arrays/maximumConsecutiveOnes.cpp
@@ -19,8 +19,6 @@ int findMaxConsecutiveOnes(int arr[], int n) {
 int main() {
     int arr[] = {1, 1, 0, 1, 1, 1, 0, 1, 1};  // Example input
     int n = sizeof(arr) / sizeof(arr[0]);
-
     cout << "Maximum Consecutive Ones: " << findMaxConsecutiveOnes(arr, n) << endl;
-
     return 0;
 }
