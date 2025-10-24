677336a8345693c99faa10d8228d10d1462386ba
Avaneesh Gupta <avaneesh.gupta5@gmail.com>
2025-10-24 20:14:53 +0530

    findUnionUsingTwoPointer

--- Arrays/findUnion.cpp
+++ Arrays/findUnion.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <set>
+#include<vector>
 using namespace std;
 
 void findUnion(int arr1[], int n, int arr2[], int m) {
@@ -23,6 +24,45 @@ void findUnion(int arr1[], int n, int arr2[], int m) {
     cout << endl;
 }
 
+void findUnionUsingTwoPointer(int arr1[], int n, int arr2[], int m) {
+    vector<int> unionArr;
+    int i = 0, j = 0;
+
+    while (i < n && j < m) {
+        if (arr1[i] < arr2[j]) {
+            if (unionArr.empty() || unionArr.back() != arr1[i])
+                unionArr.push_back(arr1[i]);
+            i++;
+        } else if (arr1[i] > arr2[j]) {
+            if (unionArr.empty() || unionArr.back() != arr2[j])
+                unionArr.push_back(arr2[j]);
+            j++;
+        } else { 
+            if (unionArr.empty() || unionArr.back() != arr1[i])
+                unionArr.push_back(arr1[i]);
+            i++;
+            j++;
+        }
+    }
+
+    while (i < n) {
+        if (unionArr.empty() || unionArr.back() != arr1[i])
+            unionArr.push_back(arr1[i]);
+        i++;
+    }
+
+    while (j < m) {
+        if (unionArr.empty() || unionArr.back() != arr2[j])
+            unionArr.push_back(arr2[j]);
+        j++;
+    }
+
+    cout << "Union of arrays using two pointers: ";
+    for (int num : unionArr) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
 int main() {
     int arr1[] = {1, 2, 3, 4, 5};
     int arr2[] = {3, 4, 5, 6, 7};
@@ -30,6 +70,7 @@ int main() {
     int m = sizeof(arr2) / sizeof(arr2[0]);
 
     findUnion(arr1, n, arr2, m);
+    findUnionUsingTwoPointer(arr1, n, arr2, m);
 
     return 0;
 }
